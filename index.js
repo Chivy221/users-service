f370c8cb439f2a799bc8a51bf65f7e4118a845e9
Chivy221 <danilius227@gmail.com>
2025-06-08 16:13:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ const cache = new NodeCache({ stdTTL: 60 });
 app.use(express.json());
 
 app.get('/ping', (_, res) => res.send('pong'));
+app.get('/health', (req, res) => {
+res.status(200).json({ status: 'ok', service: 'users-service' });
+});
 
+app.delete('/users/:id', async (req, res) => {
+try {
+const deleted = await User.findByIdAndDelete(req.params.id);
+if (!deleted) return res.status(404).send('Not found');
+res.sendStatus(204);
+} catch (e) {
+res.status(500).send('Server error');
+}
+});
 app.get('/metrics', (_, res) => {
 res.set('Content-Type', 'text/plain');
 res.send(getMetrics());
