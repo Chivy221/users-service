90a09155ad82a4ceacc6daf2b4500a637f7bb123
Chivy221 <danilius227@gmail.com>
2025-06-08 15:35:50 +0300

    Create metrics.js

---
+++ utils/metrics.js
@@ -0,0 +1,11 @@
+let requestCount = 0;
+
+function increment() {
+requestCount++;
+}
+
+function getMetrics() {
+return # HELP request_count Total requests\n# TYPE request_count counter\nrequest_count ${requestCount};
+}
+
+module.exports = { increment, getMetrics };
