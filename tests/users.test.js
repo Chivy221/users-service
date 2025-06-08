fd94b97009cd5cb6a7192f941e526eb2b9a09ac3
Chivy221 <danilius227@gmail.com>
2025-06-08 15:57:11 +0300

    Update users.test.js

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,19 +1,10 @@
 const request = require('supertest');
 const app = require('../index');
 
-describe('Users API', () => {
-it('GET /users returns array', async () => {
-const res = await request(app).get('/users');
-expect(res.statusCode).toEqual(200);
-expect(Array.isArray(res.body)).toBe(true);
-});
-
-it('POST /users creates user', async () => {
-const res = await request(app).post('/users').send({
-name: 'Test User',
-email: 'test@example.com'
-});
-expect(res.statusCode).toEqual(201);
-expect(res.body).toHaveProperty('_id');
+describe('GET /ping', () => {
+it('should return pong', async () => {
+const res = await request(app).get('/ping');
+expect(res.text).toBe('pong');
+expect(res.statusCode).toBe(200);
 });
 });
