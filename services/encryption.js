613b1fcede72a2768a055a1893e622a575d7aa29
Chivy221 <danilius227@gmail.com>
2025-06-09 18:40:00 +0300

    Update encryption.js

--- services/encryption.js
+++ services/encryption.js
@@ -1,19 +1,32 @@
-const crypto = require('crypto');
-const key = process.env.ENCRYPTION_KEY; // 32 символа
-const iv = Buffer.alloc(16, 0); // инициализационный вектор (безопасен для этого кейса)
+const crypto = require("crypto");
+
+// Ключ и IV должны быть 32 и 16 байт соответственно
+const ENCRYPTION_KEY = crypto
+  .createHash("sha256")
+  .update(String(process.env.ENCRYPTION_SECRET || "default_secret"))
+  .digest("base64")
+  .substr(0, 32);
+
+const IV_LENGTH = 16;
 
 function encrypt(text) {
-const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-let encrypted = cipher.update(text, 'utf8', 'hex');
-encrypted += cipher.final('hex');
-return encrypted;
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
 }
 
-function decrypt(encrypted) {
-const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-return decrypted;
+function decrypt(text) {
+  const textParts = text.split(":");
+  const iv = Buffer.from(textParts.shift(), "hex");
+  const encryptedText = Buffer.from(textParts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
 
 module.exports = { encrypt, decrypt };
