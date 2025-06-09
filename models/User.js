8af94f087c15748a32202b6e1e148e10aeefef53
Chivy221 <danilius227@gmail.com>
2025-06-09 15:20:26 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const UserSchema = new mongoose.Schema({
-  id: {
+  _id: {
     type: String,
-    default: uuidv4, // авто-генерация UUID
-    required: true,
-    unique: true
+    default: uuidv4
   },
   name: String,
   email: String
 });
+
 
 module.exports = mongoose.model('User', UserSchema);
